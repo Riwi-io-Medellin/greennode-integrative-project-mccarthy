b1e8dba47dc450404f167cf2dbb3d4bf9d2b53f8
Milo <jchernandez10ocm@gmail.com>
2026-03-15 21:32:24 -0500

    fix: import test conection into server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-import 'dotenv/config';
-import app from './app.js';
-import config from "./src/config/config.js"
+import "./src/config/env.js";
+import app from "./app.js";
+import { testDBConnection } from "./src/config/db.js";
+const PORT = process.env.PORT || 3000;
+
+testDBConnection();
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
